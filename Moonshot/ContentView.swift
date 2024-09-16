9861cc88079fd91c023ce070e19abcad81cec91d
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-17 00:06:37 +0300

    What we don't want to do with image

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,13 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Image("aesthetic")
+            .frame(width: 200, height: 200)
+            .clipped()
     }
 }
 
