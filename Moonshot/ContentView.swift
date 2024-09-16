0ed25ce7f0e00f6f1dfea617c08b1b1d753ab5e4
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-17 01:21:10 +0300

    Adaptive grid layout

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,16 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     let layout = [
-        GridItem(.fixed(80)),
-        GridItem(.fixed(80)),
-        GridItem(.fixed(80))
+        GridItem(.adaptive(minimum: 80))
     ]
      
     var body: some View {
         ScrollView {
             LazyVGrid(columns: layout) {
                 ForEach(0..<1000) {
-                    Text("Item \($0)") 
+                    Text("Item \($0)")
                 }
             }
         }
