1274e9cb8a0f5473f2bc23a46b1cfa444bacbeb7
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-17 01:20:35 +0300

    Fixed grid layout

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,35 +7,19 @@
 
 import SwiftUI
 
-struct User: Codable {
-    let name: String
-    let address: Address
-}
-
-struct Address: Codable {
-    let street: String
-    let city: String
-}
-
-
 struct ContentView: View {
+    let layout = [
+        GridItem(.fixed(80)),
+        GridItem(.fixed(80)),
+        GridItem(.fixed(80))
+    ]
+     
     var body: some View {
-        Button("Decode JSON") {
-            let input = """
-            {
-                "name": "David Tennant",
-                "address": {
-                    "street": "122 Doctor Street",
-                    "city": "London"
+        ScrollView {
+            LazyVGrid(columns: layout) {
+                ForEach(0..<1000) {
+                    Text("Item \($0)") 
                 }
-            }
-            """
-            
-            let data = Data(input.utf8)
-            let jsonDecoder = JSONDecoder()
-            
-            if let user = try? jsonDecoder.decode(User.self, from: data) {
-                print(user.address.street)
             }
         }
     }
