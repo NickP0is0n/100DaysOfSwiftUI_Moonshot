94533998f7978c83f2718b94aca35776f737e3af
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-17 00:42:01 +0300

    Lazy containers demo

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,12 +7,25 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(text: String) {
+        print("Creating a new CustomText.")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         ScrollView {
-            VStack(spacing: 10) {
+            LazyVStack(spacing: 10) {
                 ForEach(0..<100) {
-                    Text("Title \($0)")
+                    CustomText(text: "Title \($0)")
                         .font(.title)
                 }
             }
