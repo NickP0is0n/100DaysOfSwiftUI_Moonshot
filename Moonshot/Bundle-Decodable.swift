6f0a4c991125663169ee4f11c2b90cdb5452e0dc
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-17 13:56:44 +0300

    Loading custom Codable

---
+++ Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,36 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by Mykola Chaikovskyi on 17.09.2024.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [String: Astronaut] {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate file \(file) in the bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from the bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        do {
+            return try decoder.decode([String: Astronaut].self, from: data)
+        } catch DecodingError.keyNotFound(let key, let context) {
+            fatalError("Failed to decode \(file) from the bundle due to a missing key '\(key.stringValue)' – \(context.debugDescription).")
+        } catch DecodingError.typeMismatch(_, let context) {
+            fatalError("Failed to decode \(file) from the bundle due to a type mismatch – \(context.debugDescription)")
+        } catch DecodingError.valueNotFound(let type, let context) {
+            fatalError("Failed to decode \(file) from the bundle due to a missing \(type) value – \(context.debugDescription)")
+        } catch DecodingError.dataCorrupted(_) {
+            fatalError("Failed to decode \(file) from the bundle because it appears to be invalid JSON.")
+        } catch {
+            fatalError("Failed to decode \(file) from the bundle: \(error.localizedDescription)")
+        }
+    }
+}
