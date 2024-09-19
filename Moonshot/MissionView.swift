f541f09cf76bd2e9ded3e94493ad8a24507a3926
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-20 00:54:43 +0300

    Astronaut loading

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 struct MissionView: View {
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
     let mission: Mission
+    let crew: [CrewMember]
     
     var body: some View {
         ScrollView {
@@ -33,13 +39,26 @@ struct MissionView: View {
         .navigationBarTitleDisplayMode(.inline)
         .background(.darkBackground)
     }
+    
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+        
+        self.crew = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing \(member.name)")
+            }
+        }
+    }
 }
 
 struct MissionView_Previews: PreviewProvider {
     static var previews: some View {
         let mission: [Mission] = Bundle.main.decode("missions.json")
+        let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
         
-        MissionView(mission: mission[0])
+        MissionView(mission: mission[0], astronauts: astronauts)
             .preferredColorScheme(.dark)
     }
 }
