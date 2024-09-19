d2e1cf67666525fb0677edf55869e09721fcff09
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-20 01:04:40 +0300

    Astronauts in UI

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -32,6 +32,36 @@ struct MissionView: View {
                     Text(mission.description )
                 }
                 .padding(.horizontal)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(crew, id: \.role) { crewMember in
+                            NavigationLink {
+                                Text("Astronaut details")
+                            } label: {
+                                HStack {
+                                    Image(crewMember.astronaut.id)
+                                        .resizable()
+                                        .frame(width: 104, height: 72)
+                                        .clipShape(Capsule())
+                                        .overlay {
+                                            Capsule()
+                                                .strokeBorder(.white, lineWidth: 1)
+                                        }
+                                    VStack(alignment: .leading) {
+                                        Text(crewMember.astronaut.name)
+                                            .foregroundStyle(.white)
+                                            .font(.headline)
+                                        
+                                        Text(crewMember.role)
+                                            .foregroundStyle(.secondary)
+                                    }
+                                }
+                                .padding(.horizontal)
+                            }
+                        }
+                    }
+                }
             }
             .padding(.bottom)
         }
