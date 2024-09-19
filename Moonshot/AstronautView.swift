99f55d32f6afea96f14a63675f120736718a37ec
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-20 01:23:39 +0300

    Finished functionality-wise

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,37 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Mykola Chaikovskyi on 20.09.2024.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static var previews: some View {
+        let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+        
+        AstronautView(astronaut: astronauts["armstrong"]!)
+            .preferredColorScheme(.dark)
+    }
+}
